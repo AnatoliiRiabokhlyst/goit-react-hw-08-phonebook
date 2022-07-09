549921aea51eaa5172cfaa10595309a378642a53
Anatolii <anatoliirst@gmail.com>
2022-07-09 20:26:33 +0200

    fix

--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -1,101 +1,78 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-// import PropTypes from "prop-types";
 import FormContacts from '../FormContacts';
 import Filter from '../Filter';
 import ContactsList from '../ContactsList';
 import s from './PhoneBook.module.css';
 
-class PhoneBook extends React.Component {
-  // static propTypes = {
-  //     contacts: PropTypes.arrayOf(
-  //         PropTypes.shape({
-  //             name: PropTypes.string.isRequired,
-  //             number: PropTypes.string.isRequired,
-  //             id: PropTypes.string.isRequired,
-  //         }),
-  //     ).isRequired,
-  //     setContacts: PropTypes.func.isRequired,
-  // }
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? initialValue;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+};
+function PhoneBook() {
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useLocalStorage('filter', '');
+  const [name, setIsName] = useLocalStorage('name', '');
+  const [number, setNumber] = useLocalStorage('number', '');
 
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    name: '',
-    number: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.contacts.some(contact => contact.name === this.state.name)) {
+    if (contacts.some(contact => contact.name === name)) {
       alert('Контакт с таким именем уже существует');
       return;
     }
     const newContact = {
       id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
-    this.setState({
-      contacts: [...this.state.contacts, newContact],
-      name: '',
-      number: '',
-    });
+    setContacts([...contacts, newContact]);
+    setIsName('');
+    setNumber('');
   };
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setIsName(value);
+    } else {
+      setNumber(value);
+    }
   };
-  handleDelete = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+  const handleDelete = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
-  handleFilter = e => {
-    this.setState({
-      filter: e.target.value,
-    });
+  const handleFilter = e => {
+    setFilter(e.currentTarget.value.toLowerCase());
   };
 
-  render() {
-    const normalizeFilter = this.state.filter.toLowerCase();
-    const filterContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter)
-    );
-    return (
-      <div className={s.section}>
-        <h1 className={s.title}>Phonebook</h1>
-        <FormContacts
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-          name={this.state.name}
-          number={this.state.number}
-        />
-        <h2 className={s.title}>Contacts</h2>
-        <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
-        <ContactsList
-          contacts={filterContacts}
-          handleDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
+  const normalizeFilter = filter.toLowerCase();
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizeFilter)
+  );
+  return (
+    <div className={s.section}>
+      <h1 className={s.title}>Phonebook</h1>
+      <FormContacts
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        name={name}
+        number={number}
+      />
+      <h2 className={s.title}>Contacts</h2>
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <ContactsList contacts={filterContacts} handleDelete={handleDelete} />
+    </div>
+  );
 }
+
 export default PhoneBook;
